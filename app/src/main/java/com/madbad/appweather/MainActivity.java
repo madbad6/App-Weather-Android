6e4a8d2d21ff379fb4c3e54b6cb57b1a1d8ec2f4
Nicolay Elsukov <madbad6@yandex.ru>
2021-10-15 19:20:11 +0300

    the first commit

---
+++ app/src/main/java/com/madbad/appweather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.madbad.appweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
